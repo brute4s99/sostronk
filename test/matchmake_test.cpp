977e9fe69867c0b6bf9e834c5abe0e48c03143e9
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2020-01-25 16:46:09 +0530

    rewrite Matchmake test and add tests for new functions

--- test/matchmake_test.cpp
+++ test/matchmake_test.cpp
@@ -1,25 +1,31 @@
 #include "matchmake_test.h"
 #include <QtDebug>
+
 void MatchmakeTest::initTestCase()
 {
+    player1.first = string("Pablo Diablo");
+    player1.second = 200;
+    player2.first =string("Nicobar Escobar");
+    player2.second = 150;
+    player3.first = string("brute4s99");
+    player3.second = 400;
+    player4.first =string("orepoala");
+    player4.second = 500;
+    player5.first =string("ryuzaki3");
+    player5.second = 100;
+    team1={player1, player2};
+    team2={player3, player4};
+    team3={player2, player5};
+    match1={pair<TEAM, int>(team1,0), pair<TEAM, int>(team2, 0)};
+    match2={pair<TEAM, int>(team2,0), pair<TEAM, int>(team3, 0)};
+    match3={pair<TEAM, int>(team3,0), pair<TEAM, int>(team1, 0)};
+    matches={match1, match2, match3};
+
     // Called before the first testfunction is executed
 }
 
-void MatchmakeTest::TheTest()
+void MatchmakeTest::totalScoreTest()
 {
-    PLAYER player1(std::string("Pablo Diablo"), 200);
-    PLAYER player2(std::string("Nicobar Escobar"), 150);
-    PLAYER player3(std::string("brute4s99"), 400);
-    PLAYER player4(std::string("orepoala"), 500);
-    PLAYER player5(std::string("ryuzaki3"), 100);
-    TEAM team1{player1, player2};
-    TEAM team2{player3, player4};
-    TEAM team3{player2, player5};
-    MATCH match1{pair<TEAM, int>(team1,0), pair<TEAM, int>(team2, 0)};
-    MATCH match2{pair<TEAM, int>(team2,0), pair<TEAM, int>(team3, 0)};
-    MATCH match3{pair<TEAM, int>(team3,0), pair<TEAM, int>(team1, 0)};
-    vector< MATCH> matches{match1, match2, match3};
-
     calculateTotalScore(matches);
     QCOMPARE((matches[0])[0].second, 350);
     QCOMPARE((matches[0])[1].second, 900);
@@ -27,19 +33,46 @@ void MatchmakeTest::TheTest()
     QCOMPARE((matches[1])[1].second, 250);
     QCOMPARE((matches[2])[0].second, 250);
     QCOMPARE((matches[2])[1].second, 350);
+}
+
+void MatchmakeTest::compareMatchesTest()
+{
     QCOMPARE(compareMatches(matches[0], matches[1]), true);
     QCOMPARE(compareMatches(matches[0], matches[2]), false);
+}
 
+void MatchmakeTest::makeTeamsTest()
+{
     vector< TEAM> teams;
     makeTeams(vector< PLAYER> {player1, player2, player3, player4, player5}, teams, 4);
     vector < PLAYER> team;
-    team={player2, player3, player4, player5};
+    team = {player2, player3, player4, player5};
     QVERIFY(std::find(teams.begin(), teams.end(), team) != teams.end());
-    team={player1, player2, player4, player5};
+    team = {player1, player2, player4, player5};
     QVERIFY(std::find(teams.begin(), teams.end(), team) != teams.end());
-    team={player1, player3, player4, player5};
+    team = {player1, player3, player4, player5};
     QVERIFY(std::find(teams.begin(), teams.end(), team) != teams.end());
+}
 
+void MatchmakeTest::convertToStringTest()
+{
+    char arr[] = "Hello There";
+    QCOMPARE(typeid(convertToString(arr)) , typeid(string));
+    QVERIFY(convertToString(arr).compare(string("Hello There")) == 0);
+}
+
+void MatchmakeTest::parseScoreTest()
+{
+    string test = "Alpha Charlie 502";
+    int score = parseScore(test);
+    QCOMPARE(score, 502);
+}
+
+void MatchmakeTest::parseNameTest()
+{
+    string test = "Alpha Charlie 502";
+    string name = parseName(test);
+    QCOMPARE(name, "Alpha Charlie");
 }
 
 void MatchmakeTest::cleanupTestCase()
--- test/matchmake_test.h
+++ test/matchmake_test.h
@@ -6,12 +6,21 @@
 
 class MatchmakeTest : public QObject
 {
+    PLAYER player1, player2, player3, player4, player5;
+    TEAM team1, team2, team3;
+    MATCH match1, match2, match3;
+    vector< MATCH> matches;
     Q_OBJECT
 
 private Q_SLOTS:
     void initTestCase();
 
-    void TheTest();
+    void totalScoreTest();
+    void compareMatchesTest();
+    void makeTeamsTest();
+    void convertToStringTest();
+    void parseScoreTest();
+    void parseNameTest();
 
     void cleanupTestCase();
 
